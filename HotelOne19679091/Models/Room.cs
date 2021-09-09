be50a28f13e2d80bdd9633e68cda28919722d864
hasaniie <hasankarabork@hotmail.com>
2021-09-09 11:56:57 +1000

    creating model

---
+++ HotelOne19679091/Models/Room.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelOne19679091.Models
+{
+    public class Room
+    {
+        public int roomId { get; set; }
+
+        [Required]
+        [Display(Name ="Level")]
+        [RegularExpression(@"^[1,2,3]{1}$", ErrorMessage ="")]
+        public string level { get; set; }
+
+
+
+        [Required]
+        [Display(Name ="Beds")]
+        
+        public int bedCount { get; set; }
+
+    }
+}
