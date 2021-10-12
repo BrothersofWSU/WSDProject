7763c5702898986d2388b9068296292c9560dc21
hasaniie <hasankarabork@hotmail.com>
2021-10-12 22:59:01 +1100

    SeedRoles.cs, assigned one admin. all others are assigned customers role

--- HotelOne19679091/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ HotelOne19679091/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -91,6 +91,9 @@ namespace HotelOne19679091.Areas.Identity.Pages.Account
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
+                    //Add this user to the role Customers
+                    await _userManager.AddToRoleAsync(user, "Customers");
+
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
                         return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
--- HotelOne19679091/Program.cs
+++ HotelOne19679091/Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HotelOne19679091.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace HotelOne19679091
 {
@@ -13,7 +15,30 @@ namespace HotelOne19679091
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+           //create the WebHost object; the method is declared in the bottom
+           var host = CreateHostBuilder(args).Build();
+
+           using (var scope = host.Services.CreateScope())
+            {
+                //get the services providers
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var serviceProvider = services.GetRequiredService<IServiceProvider>();
+                    // get the Config object for the appsettings.json file 
+                    var configuration = services.GetRequiredService<IConfiguration>();
+                    // pass the service proiders and the config object to CreateRoles()
+                    SeedRoles.CreateRoles(serviceProvider, configuration).Wait();
+                }
+                catch(Exception exception)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(exception, "An error occurred while creating roles");
+                }
+            }
+            //start the web app
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
